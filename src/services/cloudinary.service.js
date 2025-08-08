457a2a5202a3d50ec940efbd71cdb5c2f5fffa84
Mohd Asif <37485301+aansari4599@users.noreply.github.com>
2025-08-09 03:05:17 +0530

    multer config

---
+++ src/services/cloudinary.service.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return `Could not find the path ${localFilePath}`;
+    // upload the file using localfilepath
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // uploaded successfully
+    console.log("file uploaded successfully...", response.url);
+    return response;
+  } catch (error) {
+    //remove the locally saved temporary file as the upload operation  got failed
+    fs.unlinkSync(localFilePath);
+    return `file could not get uploaded...`;
+  }
+};
+export { uploadOnCloudinary };
