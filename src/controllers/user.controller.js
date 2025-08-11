663d38ca903a55cbb4cc64f5a3a53b4d65d08a93
Mohd Asif <37485301+aansari4599@users.noreply.github.com>
2025-08-11 06:26:38 +0530

    auth added with access token and refresh token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,24 @@
+import jwt from "jsonwebtoken";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../services/cloudinary.service.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false }); // no need to save in a variable
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "tokens could not be generated...");
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   // Algorithm to register a user
@@ -17,7 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "all fields are required");
 
   // check if the user already exists -> either username or email or both
-  const userExists = User.findOne({
+  const userExists = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (userExists)
@@ -36,7 +52,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // create user object and create entry in db and login
   const user = await User.create({
     fullname,
-    avatar: avatar.url,
+    avatar: avatar?.url,
     coverImage: coverImage?.url || "",
     email,
     password,
@@ -56,4 +72,130 @@ const registerUser = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(200, createdUser, "User registered successfully..."));
 });
-export { registerUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  // TODOS:
+  //req body => data
+  const { email, username, password } = req.body;
+
+  if (!(username || email)) {
+    throw new ApiError(400, "username or email is required...");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  // check user exists or not -> username ya email m se koi ek
+
+  if (!user) {
+    throw new ApiError(404, "User not found...");
+  }
+  // password verification
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid User Credentials...");
+  }
+  // check access token and refresh token , generate them and send cookies along with them
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  // find user -> userId -> to find currently logged in user is the challenge here
+  // to take on this problem we need to implement our custom middleware to extract the data that is required to fetch the user details from database.
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true, // returns updated value
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id).select("-password");
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh token is expired or used");
+    }
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    const { newAccessToken, newRefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
+    return res
+      .status(200)
+      .cookie("accessToken", newAccessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          {
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken,
+          },
+          "Access token refreshed"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token");
+  }
+});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken };
--- src/services/cloudinary.service.js
+++ src/services/cloudinary.service.js
@@ -13,14 +13,20 @@ const uploadOnCloudinary = async (localFilePath) => {
     // upload the file using localfilepath
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // added here as the code was running asynchronosly, the issue ws due to import syntax the apinkey ws not available at the time the config was getting set. config was getting loaded later on while uploading so decided to pass config at the time of uploading the file.
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     });
 
     // uploaded successfully
+    fs.unlinkSync(localFilePath);
     console.log("file uploaded successfully...", response.url);
     return response;
   } catch (error) {
     //remove the locally saved temporary file as the upload operation  got failed
     fs.unlinkSync(localFilePath);
+    console.log("==> ", process.env.CLOUDINARY_API_KEY);
+    console.log("-->", error);
     return `file could not get uploaded...`;
   }
 };
