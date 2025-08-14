f8c0e2e27a288b22e71f05499822beb8bcb354da
Mohd Asif <37485301+aansari4599@users.noreply.github.com>
2025-08-15 04:41:44 +0530

    debugged issue

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,8 +1,9 @@
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-  res.send(200).json(new ApiResponse(200, null, "healthcheck OK!"));
+  return res.status(200).json(new ApiResponse(200, null, "healthcheck OK!"));
 });
 
 export { healthcheck };
